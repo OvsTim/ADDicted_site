508a7588fbc37d8152cfcd0620a054a49effb792
ovstim <ovstim@mail.ru>
2024-01-10 12:07:58 +0300

    #restyle header, added alt logo, gallery, changed mainphoto

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import bigImage from '../public/image.jpg'
+import Gallery from "@/components/Gallery";
 export default function Home() {
 
   return (
-      <main className="w-full h-full m-auto bg-black flex justify-center">
+      <main className="m-auto bg-black flex flex-col place-items-center">
+              <Image
+                  className={'bg-black opacity-65 w-full lg:w-[1024px] my-10'}
+                  src={bigImage} alt={'title'}/>
+        <Gallery/>
 
-          <Image
-              className={'bg-black w-full lg:w-[1024px] my-10'}
-              src={bigImage} alt={'title'}/>
       </main>
   )
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,8 @@
-'use client'
 import Link from "next/link";
 import React, { useState } from "react";
 import Image from "next/image";
-import logo from '../public/logo.jpg'
+import logo from '../public/logo_white.png'
+import logo_horizontal from '../public/logo_horiziotal.png'
 import { ImMusic } from "react-icons/im";
 import { ImBubble2 } from "react-icons/im";
 import { ImCalendar } from "react-icons/im";
@@ -10,52 +10,56 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className={'bg-black w-full m-auto h-full  flex place-content-between flex-row'}>
-                <Image
-                    className={'aspect-auto w-[100px] md:w-[125px] lg:w-[150px]'}
-                    src={logo} alt={'logo'}/>
-                <div className={'w-1/2 bg-red-600 hidden place-content-evenly place-items-center md:flex'}>
-                    <Link
-                        className={'text-white text-sm flex flex-row place-items-center lg:text-xl'}
-                        href={'/'}>
-                        <ImCalendar/>
-                        <span className={'ml-2.5'}>Концерты</span>
-                    </Link>
-                    <Link
-                        className={'text-white text-sm flex flex-row place-items-center lg:text-xl'}
-                        href={'/'}>
-                        <ImMusic/>
-                        <span className={'ml-2.5'}>Музыка</span>
-                    </Link>
-                    <Link
-                        className={'text-white text-sm flex flex-row place-items-center lg:text-xl'}
-                        href={'/'}>
-                        <ImBubble2/>
-                        <span className={'ml-2.5'}>Контакты</span>
-                    </Link>
-                </div>
+             <div className={'bg-black m-auto h-full flex flex-col place-items-center md:flex-row md:place-content-between border-b-[1px] '}>
+                 <Image
+                     className={'w-10/12 p-[50px] md:hidden'}
+                     src={logo_horizontal} alt={'logo'}/>
+                 <Image
+                     className={'m-2 p-5 aspect-auto w-[100px] hidden md:w-[125px] md:flex lg:w-[150px]'}
+                     src={logo} alt={'logo'}/>
+                 <div className={'w-2/3 h-full py-10 hidden place-content-evenly place-items-center md:flex'}>
+                     <Link
+                         className={'text-white text-xl flex flex-row place-items-center'}
+                         href={'/'}>
+                         <ImCalendar size={24}/>
+                         <span className={'mx-2.5 text-2xl uppercase'}>Концерты</span>
+                     </Link>
+                     <Link
+                         className={'text-white text-xl flex flex-row place-items-center'}
+                         href={'/'}>
+                         <ImMusic size={24}/>
+                         <span className={'mx-2.5 text-2xl uppercase'}>Музыка</span>
+                     </Link>
+                     <Link
+                         className={'text-white text-xl flex flex-row place-items-center'}
+                         href={'/'}>
+                         <ImBubble2 size={24}/>
+                         <span className={'mx-2.5 text-2xl uppercase'}>Контакты</span>
+                     </Link>
+                 </div>
 
-            </div>
-            <div className={'w-full space-y-2 py-2 bg-red-600 place-content-evenly place-items-center flex flex-col md:hidden'}>
+             </div>
+            <div
+                className={'w-full space-y-7 py-5 bg-black place-content-evenly place-items-center flex flex-col md:hidden'}>
                 <Link
                     className={'text-white text-xl flex flex-row place-items-center'}
                     href={'/'}>
-                    <ImCalendar/>
-                    <span className={'ml-2.5'}>Концерты</span>
+                    <ImCalendar size={16}/>
+                    <span className={'ml-2.5 uppercase'}>Концерты</span>
                 </Link>
                 <Link
                     className={'text-white text-xl flex flex-row place-items-center'}
                     href={'/'}>
-                    <ImMusic/>
-                    <span className={'ml-2.5'}>Музыка</span>
+                    <ImMusic size={16}/>
+                    <span className={'ml-2.5 uppercase'}>Музыка</span>
                 </Link>
                 <Link
                     className={'text-white text-xl flex flex-row place-items-center'}
                     href={'/'}>
-                    <ImBubble2/>
-                    <span className={'ml-2.5'}>Контакты</span>
-
+                    <ImBubble2 size={16}/>
+                    <span className={'ml-2.5 uppercase'}>Контакты</span>
                 </Link>
+                <hr className={'w-full bg-white h-[1px]'}/>
             </div>
         </div>
     );
