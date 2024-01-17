f07231eaae2a8a6b6b105420517c647eb239c253
ovstim <ovstim@mail.ru>
2024-01-17 21:20:58 +0300

    mobile && zindex fixes

--- app/page.tsx
+++ app/page.tsx
@@ -23,14 +23,14 @@ export default function Home() {
         <main className='m-auto flex flex-col place-items-center bg-black'>
             <div className="h-full w-[100%] relative lg:w-[1024px] py-5">
                 <Slider/>
-                <div className="z-40 absolute bottom-0 px-4 py-3 w-full h-full space-y-20 place-items-center md:place-items-start justify-center flex flex-col lg:w-[1024px]">
-                    <span className="text-white font-bold text-4xl mx-2.5 my-2.5"> Новый альбом/тур 2024</span>
+                <div className="z-40 absolute bottom-0 px-4 py-3 w-full h-full space-y-5 place-items-center md:place-items-start justify-center flex flex-col lg:w-[1024px] md:space-y-20">
+                    <span className="text-white font-bold text-2xl mx-2.5 my-2.5 md:text-4xl"> Новый альбом/тур 2024</span>
                     <div className={'flex flex-col space-y-5 space-x-0 place-items-center w-full md:w-1/2 md:flex-row justify-center h-fit md:space-y-0 md:space-x-5'}>
                         <Link
                             id={'listenAlbum'}
                             href={'#album'}
                             className={
-                                ' text-xl bg-gray-500/50 w-2/3 flex flex-row place-items-center justify-around border-2 border-white py-3 px-5 uppercase text-white hover:bg-white hover:text-black md:w-full '
+                                'bg-gray-500/50 w-2/3 flex flex-row place-items-center justify-around border-2 border-white py-3 px-5 uppercase text-white hover:bg-white hover:text-black md:w-full md:text-xl'
                             }
                         >
                             Послушать
@@ -39,7 +39,7 @@ export default function Home() {
                             id={'buyTickets'}
                             href={'#concerts'}
                             className={
-                                ' text-xl bg-gray-500/50 w-2/3 flex flex-row place-items-center justify-around border-2 border-white py-3 px-5 uppercase text-white hover:bg-white hover:text-black md:w-full'
+                                'bg-gray-500/50 w-2/3 flex flex-row place-items-center justify-around border-2 border-white py-3 px-5 uppercase text-white hover:bg-white hover:text-black md:w-full md:text-xl'
                             }
                         >
                             Увидеть
@@ -60,7 +60,7 @@ export default function Home() {
         </main>
         <button
             aria-label="go up"
-            className='fixed bottom-20 right-10 rounded-3xl bg-gray-300 p-2.5 md:invisible'
+            className='fixed bottom-20 right-10 rounded-3xl bg-gray-300 p-2.5 md:invisible z-50'
             onClick={scrollToTop}
         >
             <FaArrowUp/>
